12c8584c1ec8cfe5a4f4b602eff19f951514005a
Tushar Mahale <tushar.mahale11@gmail.com>
2020-07-25 18:13:32 +0530

    Adding unit tests

---
+++ unitTest.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import unittest
+import app
+
+class TestService(unittest.TestCase):
+  def setUp(self):
+    app.app.testing = True
+    self.app = app.app.test_client()
+
+  def test_service(self):
+    rv = self.app.get('/')
+    self.assertEqual(rv.status, '200 OK')
+
+  def test_stats(self):
+    rv = self.app.get('/stats')
+    self.assertEqual(rv.status, '200 OK')
+
+  def test_api(self):
+    rv = self.app.get('/api')
+    self.assertEqual(rv.status, '200 OK')
+
+  def test_health(self):
+    rv = self.app.get('/health')
+    self.assertEqual(rv.status, '200 OK')
+    self.assertEqual(rv.data, b'OK')
+
+if __name__ == '__main__':
+  unittest.main()
